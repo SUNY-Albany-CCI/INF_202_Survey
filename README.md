171aae5a0a031ab154b5a65b9c0186443deb81ce
Luis Ibanez <luis.ibanez@kitware.com>
2013-01-26 22:13:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This project implements a Survey to illustrate a
+process of Data gathering and Data Analysis for
+the INF 202 Database class.
+
+This Web Application is implemented using Web2py.
+
+This project is distributed under the Apache 2.0 License.
+
+Please see NOTICE and LICENSE files for details.
+
---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'INF 202 Survey'
+settings.subtitle = 'powered by web2py'
+settings.author = 'Luis Ibanez'
+settings.author_email = 'luis.ibanez@kitware.com'
+settings.keywords = ''
+settings.description = 'Survey to use in INF 202 class'
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '5e2e4513-11c9-44b6-88b2-b2a8a7a87d4d'
+settings.email_server = 'localhost'
+settings.email_sender = 'you@example.com'
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = ['jqmobile', 'translate']
